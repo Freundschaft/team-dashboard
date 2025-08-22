603335f66301554bc34be6e4bb683746b376d311
Qiong Wu <qiong.wu@gfnork.de>
2025-08-22 19:45:10 +0200

    remove unnecessary jest config

--- jest.config.ts
+++ jest.config.ts
@@ -13,22 +13,8 @@ const config = {
     '<rootDir>/tests/**/*.test.ts',
     '<rootDir>/tests/**/*.spec.ts'
   ],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{js,ts}',
-    '!<rootDir>/src/**/*.d.ts',
-    '!<rootDir>/src/**/types/**',
-    '!<rootDir>/src/**/node_modules/**'
-  ],
-  coverageDirectory: '<rootDir>/tests/coverage',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   testTimeout: 10000, // 10 seconds for database tests
-  verbose: true,
-  collectCoverage: false, // Set to true when you want coverage reports
-
-  // Handle TypeScript imports in tests
-  transform: {
-    '^.+\\.(js|ts)$': 'babel-jest'
-  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
