9563cf350f070349a436c180f044d0a85c741dc5
Qiong Wu <qiong.wu@gfnork.de>
2025-08-23 00:19:25 +0200

    add component test stub

--- jest.config.ts
+++ jest.config.ts
@@ -7,15 +7,40 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'node',
-  testMatch: [
-    '<rootDir>/tests/**/*.test.ts',
-    '<rootDir>/tests/**/*.spec.ts'
+  projects: [
+    {
+      displayName: 'dom',
+      testEnvironment: 'jsdom',
+      testMatch: [
+        '<rootDir>/tests/component/*.test.tsx',
+        '<rootDir>/tests/component/*.spec.tsx'
+      ],
+      transform: {
+        '^.+\\.(tsx?|jsx?)$': ['babel-jest', { presets: ['next/babel'] }],
+      },
+      moduleDirectories: ['node_modules', '<rootDir>/src'],
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+    },
+    {
+      displayName: 'node',
+      testEnvironment: 'node',
+      testMatch: [
+        '<rootDir>/tests/database/*.test.ts',
+        '<rootDir>/tests/database/*.spec.ts'
+      ],
+      setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+    },
   ],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  coverageProvider: 'v8',
   testTimeout: 10000, // 10 seconds for database tests
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+
 export default createJestConfig(config)
